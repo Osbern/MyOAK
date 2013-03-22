fb284f0cac2e7cb179fd35e212fb3429e0354723
lildrame1 <lildrame1@gmail.com>
2013-03-22 12:03:21 +0100

    Creating business classes

---
+++ MyOAK/src/com/plow/myoak/business/files/File.java
@@ -0,0 +1,33 @@
+package com.plow.myoak.business.files;
+
+import java.util.List;
+
+public class File implements Resource {
+ 
+
+	@Override
+	public ResourceType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void open() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Resource rename() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
